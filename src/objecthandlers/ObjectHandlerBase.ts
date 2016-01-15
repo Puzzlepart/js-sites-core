eff0c569b09dcf1f759878d44d14a329e4fa126a
olemp <olemp@puzzlepart.com>
2016-01-15 15:05:56 +0100

    1.0.2

---
+++ src/objecthandlers/ObjectHandlerBase.ts
@@ -0,0 +1,8 @@
+/// <reference path="IObjectHandler.ts" />
+
+module Pzl.Sites.Core.ObjectHandlers {
+    export class ObjectHandlerBase implements IObjectHandler {
+        name: string;
+        ProvisionObjects(obj) {}
+    }
+}
